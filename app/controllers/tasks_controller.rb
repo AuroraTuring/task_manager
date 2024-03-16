ae02ea4cbc9cd316a091b90968027c3b6d0f3e45
Aurora Sanderson <AuroraTuring@gmail.com>
2024-03-16 15:43:28 -0600

    Changes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,13 @@
+# app/controllers/tasks_controller.rb
+
 class TasksController < ApplicationController
   def index
     @tasks = ['Task 1', 'Task 2', 'Task 3']
   end
 
   def new
+  end
+
+  def create
   end
 end
